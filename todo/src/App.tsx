67e437061b7b897a34a76295e3ef249157f2d8a1
Anush Rawat <anushra0701@gmail.com>
2022-08-18 10:28:30 +0530

    added routes

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,17 +1,43 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./Pages/Home";
 import Heading from "./Components/Heading/Heading";
 import List from "./Components/List/List";
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <div className="App">
-          <Heading />
-          <List />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/todo">Todo App</Link>
+              </li>
+              <li>
+                <Link to="/bookmark">Bookmark App</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/todo"
+              element={
+                <>
+                  <Heading /> <List />{" "}
+                </>
+              }
+            ></Route>
+            <Route path="/bookmark"></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
         </div>
-      </>
+      </Router>
     );
   }
 }
